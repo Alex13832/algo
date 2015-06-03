594221e3549476472deaed82ded8b4e216938283
alex011235 <st.alexander85@gmail.com>
2015-06-04 01:44:45 +0200

    DFS implemented

---
+++ graph/dfs.cc
@@ -0,0 +1,27 @@
+//DFS implementation using a stack
+#include <vector>
+#include <iostream>
+#include "graph.h"
+using namespace std;
+
+std::vector<int> Graph::DFS(int s) {
+	vector<bool> discovered(V,false);
+	vector<int> res;
+	vector<int> stack;
+	stack.push_back(s);
+	while(!stack.empty()){
+		int v = stack.back();
+		res.push_back(v);
+		stack.pop_back();
+		cout << v << " ";
+		if(!discovered[v]) {
+			discovered[v] = true;
+
+			for (auto i = adj[v].rbegin(); i != adj[v].rend(); ++i) {
+				stack.push_back(*i);
+			}
+		}	
+	}	
+
+	return res;
+}
