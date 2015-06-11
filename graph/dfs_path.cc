fe4ae4219758ed69aede4356fe5f60e741dbfe9e
alex011235 <st.alexander85@gmail.com>
2015-06-11 12:42:32 +0200

    pathDFS added

---
+++ graph/dfs_path.cc
@@ -0,0 +1,40 @@
+#include "graph.h"
+#include <algorithm>
+using namespace std;
+
+pair<vector<int>,bool> pathDFS(Graph G, int s, int t) {
+	vector<bool> discovered(G.size(),false);
+	vector<int> stack;
+	stack.push_back(s);
+	vector<int> prev(G.size(),0);
+
+	while(!stack.empty() && s != t){
+		int v = stack.back();
+		stack.pop_back();
+
+		if(!discovered[v]) {
+			discovered[v] = true;
+
+			for (auto i = G.adj[v].rbegin(); i != G.adj[v].rend(); ++i) {
+				stack.push_back(*i);
+				prev[*i] = v;
+			}
+		}	
+	}	
+	
+	vector<int> path{t};
+	int next = t;
+	while (next != s) {
+		next = prev[next];
+		path.push_back(next);
+	}
+
+	reverse(path.begin(),path.end());
+	bool ispath = true;
+
+	if ((path.back()-path.front() > 1) && path.size() == 2) {
+		ispath = false;
+	}
+	
+	return pair<vector<int>,bool>(path,ispath);
+}
