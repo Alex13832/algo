9ace31e95007ab21aad59c9cf64682e04d5f76a9
alex011235 <st.alexander85@gmail.com>
2015-06-27 11:04:55 +0200

    first commit edf

---
+++ scheduling/edf.cc
@@ -0,0 +1,34 @@
+/** -----------------------------------------------------------
+* Optimal scheduling implementation. Earliest deadline first. 
+* Alexander Karlsson, 2015-06-27
+* ------------------------------------------------------------- */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct job { 
+	long start, finish;
+	string label; // Not compulsory
+};
+
+struct job_comp {
+  bool operator() (const  job j1, const job j2) const
+  {return j1.finish < j2.finish;}
+};
+
+/* Optimal scheduling */
+vector<job> edf(vector<job> jobs) {
+	vector<job> opt_jobs;	
+	sort(jobs.begin(),jobs.end(),job_comp());
+	
+	while (!jobs.empty()) {
+		job jb = jobs[0];
+		opt_jobs.push_back(jb);
+		
+		jobs.erase(remove_if(jobs.begin(),jobs.end(),[&](job& j){
+			return j.start < jb.finish;}),jobs.end());
+	}
+
+	return opt_jobs;
+}
