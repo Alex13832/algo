262174b233e87458cdd937fe4bd4df12f30704a4
alex011235 <st.alexander85@gmail.com>
2015-07-04 23:12:32 +0200

    Triangulation algorithm

---
+++ geometry/triangulation.cc
@@ -0,0 +1,59 @@
+/** --------------------------------------------------------------
+* Implementation of an incremental triangulation algorithm.
+* it takes a vector of points as input. On test-computer: slow
+* for inputs larger than 10 000.
+* Alexander Karlsson, 2015-06-04
+* ---------------------------------------------------------------- */
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+#include "geometry2d.cc"
+using namespace std;
+
+/** Incremental triangulation algorithm 
+ N.B. pts has to be lexicographical sorted.*/
+vector<edge> triangulation(vector<point>& pts) {
+	vector<point> visited{pts[0],pts[1],pts[2]};
+	bool intersection = false;
+
+	edge e0,e1,e2;
+	e0.p1 = pts[0]; e0.p2 = pts[1];
+	e1.p1 = pts[0]; e1.p2 = pts[2];
+	e2.p1 = pts[1]; e2.p2 = pts[2];
+	vector<edge> edges{e0,e1,e2};
+
+	for (size_t i = 0; i < pts.size(); ++i){
+		point& p0 = pts[i];
+	
+		for (auto pi = visited.rbegin(); pi!=visited.rend(); ++pi) {
+
+			for (auto e = edges.rbegin(); e!=edges.rend(); ++e) {
+				point pk = e->p1;
+				point pj = e->p2;
+
+				if (segment_intersect(p0,*pi,pj,pk)) {
+					intersection = true;
+					break;
+				}
+			}		
+
+			if (!intersection){
+				edge e1;
+				e1.p1 = p0;
+				e1.p2 = *pi;
+
+				if (p0 != *pi && find(edges.begin(),edges.end(),e1) == edges.end()) {
+					 edges.push_back(e1);
+				}
+			}
+
+			intersection = false;
+		}
+
+		visited.push_back(p0);
+	}
+
+	cout << "done" << endl;
+	return edges;
+}
