a8e9f241694dda6e5fe02f36dd44fd005bdad228
= <=>
2016-03-14 21:41:20 +0100

    bfs and dijkstra updated

--- graph/bfs.cc
+++ graph/bfs.cc
@@ -1,27 +1,39 @@
-//BFS implemented using a queue
-#include "../data_structures/graph.h"
+// New implementation of BFS!
+// Instead of Graph data strucutre now the graph is a
+// vector<vector<pair<int,int>>> structure
+//
+// Alexander Karlsson 2016-03-14
+
 #include <vector>
+#include <iostream>
+#include <list>
+#include <climits>
 using namespace std;
 
-vector<int> BFS(Graph G, int s){
-	vector<bool> visited(G.size(),false);
-	list<int> q;
-	vector<int> res;
-	visited[s] = true;
-	q.push_back(s);
+/* Breadth first search algorithm */
+vector<int> BFS(vector<vector<pair<int,int>>> G, int s){
+	vector<int> distance(G.size(),INT_MAX);
+    vector<int> parent(G.size(),-1);
+
+    list<int> q;
+
+    distance[s] = 0;
+    q.push_back(s);
+
 
 	while(!q.empty()) {
-		s = q.front();
-		res.push_back(s);
-		q.pop_front();
 
-		for (auto i = G.adj[s].begin(); i != G.adj[s].end(); ++i) {
-			if(!visited[*i]) {
-				visited[*i] = true;
-				q.push_back(*i);
-			}
-		}			
+        int curr = q.front();
+        q.pop_front();
+
+        for (auto n: G[curr]) {
+            if (distance[n.first] == INT_MAX) {
+                distance[n.first] = distance[curr] + 1;
+                parent[n.first] = curr;
+                q.push_back(n.first);
+            }
+        }
 	}
-	
-	return res;
+
+	return distance;
 }
--- graph/dijkstra_pq.cc
+++ graph/dijkstra_pq.cc
@@ -1,19 +1,35 @@
-//An implementation of Dijktra's shortest path algorithm 
-//using a priority queue.
-#include "../data_structures/graph.h"
+// Updated implementation of Dijkstra's algorithm
+// Is still using a priority queue.
+// Graph structure: vector<vector<pair<int,int>>> adj
+//
+// Undirected graph:
+//            adj[x].push_back(make_pair(y, weight));
+//            adj[y].push_back(make_pair(x, weight));
+//
+// Alexander Karlsson 2016-03-14
+
 #include <vector>
 #include <queue>
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include <map>
 using namespace std;
+
+#define UINT_MAX 4294967295U
 
 struct comp_dijkstra {
   bool operator() (const pair<uint,uint>& lhs, const pair<uint,uint>& rhs) const
   {return lhs.second>rhs.second;}
 };
 
-vector<uint> dijkstra_pq(Graph G, const int s) {
-	priority_queue<pair<uint,uint>, vector<pair<uint,uint>>,comp_dijkstra> pq;
-	vector<uint> dist(G.size(),UINT_MAX);
-	vector<int> prev(G.size(),0);
+vector<uint> dijkstra_pq(vector<vector<pair<int, int> >> G, const int s)
+{
+    priority_queue<pair<uint,uint>, vector<pair<uint,uint>>,comp_dijkstra> pq;
+
+    vector<uint> dist(G.size(),UINT_MAX);
+    vector<int> prev(G.size(),0);
+
 	dist[s] = 0;
 	pq.push(pair<uint,uint>(s,0));
 
@@ -21,15 +37,15 @@ vector<uint> dijkstra_pq(Graph G, const int s) {
 		pair<uint,uint> U = pq.top();
 		pq.pop();
 		uint u = U.first;
-		
-		for (auto v: G.adj[u]) {
-			uint alt = dist[u] + G.getEdgeWeight(u,v);
-			if (alt < dist[v]) {
-				dist[v] = alt;
-				prev[v] = u;	
-				pq.push(pair<int,int>(v,alt));
+
+		for (auto v: G[u]) {
+			uint alt = dist[u] + v.second;
+			if (alt < dist[v.first]) {
+				dist[v.first] = alt;
+				prev[v.first] = u;
+				pq.push(pair<int,int>(v.first,alt));
 			}
-		}	
+		}
 	}
 
 	return dist;
