a619569a7553edd5ef9b700d3641840c83415e26
Alexander Karlsson <st.alexander85@gmail.com>
2019-06-22 10:15:36 +0200

    Make Bitap more compact

--- strings/bitap.cc
+++ strings/bitap.cc
@@ -15,26 +15,18 @@ string bitap_search(string text, string pattern)
 {
     size_t m = pattern.length();
 
-    if (m == 0)
-    {
-        return text;
-    }
+    if (m == 0) return text;
 
     vector<int> R(m, 0);
     R[0] = 1;
 
-    for (size_t i = 0; i < text.length(); i++)
-    {
+    for (size_t i = 0; i < text.length(); i++) {
         // Uppdate the bit array 
-        for (size_t k = m; k >= 1; k-= 1)
-        {
+        for (size_t k = m; k >= 1; k-= 1) {
             R[k] = R[k-1] & (text[i] == pattern[k-1]);
         } 
 
-        if (R[m] > 0)
-        {
-            return text.substr(i - m + 1);
-        }
+        if (R[m] > 0) return text.substr(i - m + 1);
     }
 
     return "NULL";
