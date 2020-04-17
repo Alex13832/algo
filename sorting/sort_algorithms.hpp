525f42e10368adf100695a0f05d2b08d70a9fc51
Alexander Karlsson <st.alexander85@gmail.com>
2020-04-17 17:53:30 +0200

    Add external links to Wikipedia.

--- sorting/sort_algorithms.hpp
+++ sorting/sort_algorithms.hpp
@@ -22,12 +22,14 @@ namespace Sort {
 /// \brief Bubble sort.
 /// \tparam T Type in vector.
 /// \param lst The list to sort.
+/// \link <a href="https://en.wikipedia.org/wiki/Bubble_sort">Bubble sort, Wikipedia.</a>
 template<typename T>
 void BubbleSort(std::vector<T> &vec);
 
 /// \brief Bucket sort algorithm.
 /// \tparam T Type in vector.
 /// \param vec the vector to be sorted.
+/// \link <a href="https://en.wikipedia.org/wiki/Bucket_sort">Bucket sort, Wikipedia.</a>
 template<typename T>
 void BucketSort(std::vector<T> &vec);
 
@@ -35,12 +37,14 @@ void BucketSort(std::vector<T> &vec);
 /// \tparam T Type in vector.
 /// \param lst The list to sort.
 /// \return A sorted list.
+/// \link <a href="https://en.wikipedia.org/wiki/Merge_sort">Merge sort, Wikipedia.</a>
 template<typename T>
 void MergeSort(std::vector<T> &lst);
 
 /// \brief Quick-sort algorithm.
 /// \tparam T Type to be sorted.
 /// \param vec Input vector to sort.
+/// \link <a href="https://en.wikipedia.org/wiki/Quicksort">Quicksort, Wikipedia.</a>
 template<typename T>
 void QuickSort(std::vector<T> &vec);
 } // namespace Sorting
