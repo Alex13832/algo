12859b448319b723ec7a68fe9440510275b4bf12
Alexander Karlsson <st.alexander85@gmail.com>
2020-06-22 18:12:25 +0200

    Update travis

--- test/test_algo_network.cpp
+++ test/test_algo_network.cpp
@@ -83,54 +83,54 @@ TEST(test_algo_network, pagerank_8x8)
   EXPECT_TRUE(sum > 0.99 && sum < 1.01);
 }
 
-//TEST(test_algo_network, pagerank_50)
-//{
-//  // Testfile downloaded from http://introcs.cs.princeton.edu/java/16pagerank/
-//  // This test use link counts.
-//  ifstream infile(kFilePath + "medium.txt");
-//  string line;
-//  int num = 0;
-//  getline(infile, line);
-//  size_t N = stoi(line);
-//
-//  Mat M(N, Arr(N));
-//  std::vector<int> degree(N);
-//
-//  while (getline(infile, line)) {
-//    vector<int> temp;
-//    string s = line;
-//    string delimiter = " ";
-//    size_t pos = 0;
-//    string token;
-//    while ((pos = s.find(delimiter)) != string::npos) {
-//      token = s.substr(0, pos);
-//      try {
-//
-//        num = stoi(token);
-//        temp.push_back(num);
-//
-//      } catch (std::invalid_argument&) {
-//      }
-//      s.erase(0, pos + delimiter.length());
-//    }
-//
-//    try {
-//      temp.push_back(stod(s));
-//    } catch (std::invalid_argument& e) {
-//      cout << e.what() << endl;
-//    }
-//
-//    for (size_t i = 0; i < temp.size(); i += 2) {
-//      M[temp[i]][temp[i + 1]]++;
-//    }
-//  }
-//
-//  for (size_t i = 0; i < N; i++) {
-//    degree[i] = accumulate(M[i].begin(), M[i].end(), 0.0);
-//  }
-//
-//  Mat p{MatTransition(M, degree)};
-//  Arr rank{PageRank(p, 0.0001, 0.85)};
-//  double sum{accumulate(rank.begin(), rank.end(), 0.0)};
-//  EXPECT_TRUE(sum > 0.99 && sum < 1.01);
-//}
+TEST(test_algo_network, pagerank_50)
+{
+  // Testfile downloaded from http://introcs.cs.princeton.edu/java/16pagerank/
+  // This test use link counts.
+  ifstream infile(kFilePath + "medium.txt");
+  string line;
+  int num = 0;
+  getline(infile, line);
+  size_t N = stoi(line);
+
+  Mat M(N, Arr(N));
+  std::vector<int> degree(N);
+
+  while (getline(infile, line)) {
+    vector<int> temp;
+    string s = line;
+    string delimiter = " ";
+    size_t pos = 0;
+    string token;
+    while ((pos = s.find(delimiter)) != string::npos) {
+      token = s.substr(0, pos);
+      try {
+
+        num = stoi(token);
+        temp.push_back(num);
+
+      } catch (std::invalid_argument&) {
+      }
+      s.erase(0, pos + delimiter.length());
+    }
+
+    try {
+      temp.push_back(stod(s));
+    } catch (std::invalid_argument& e) {
+      cout << e.what() << endl;
+    }
+
+    for (size_t i = 0; i < temp.size(); i += 2) {
+      M[temp[i]][temp[i + 1]]++;
+    }
+  }
+
+  for (size_t i = 0; i < N; i++) {
+    degree[i] = accumulate(M[i].begin(), M[i].end(), 0.0);
+  }
+
+  Mat p{MatTransition(M, degree)};
+  Arr rank{PageRank(p, 0.0001, 0.85)};
+  double sum{accumulate(rank.begin(), rank.end(), 0.0)};
+  EXPECT_TRUE(sum > 0.99 && sum < 1.01);
+}
